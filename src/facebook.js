8f09a1eca994ce6a9ad37413ccf638133e004678
Nagy Máté <nagy.mate.0125@gmail.com>
2021-05-23 17:16:13 +0200

    preload facebook sdk

--- src/facebook.js
+++ src/facebook.js
@@ -126,11 +126,19 @@ class FacebookLogin extends React.Component {
     const { language } = this.props;
     ((d, s, id) => {
       const element = d.getElementsByTagName(s)[0];
+      console.log({element});
       const fjs = element;
       let js = element;
+      console.log({d});
+      console.log({s});
+      console.log({id});
       if (d.getElementById(id)) { return; }
-      js = d.createElement(s); js.id = id;
-      js.src = `https://connect.facebook.net/${language}/sdk.js`;
+      js = d.createElement('link');
+      js.href = `https://connect.facebook.net/${language}/sdk.js`;
+      js.rel = 'preload';
+      js.as = 'script';
+      js.id = id;
+      // js.src = `https://connect.facebook.net/${language}/sdk.js`;
       fjs.parentNode.insertBefore(js, fjs);
     })(document, 'script', 'facebook-jssdk');
   }
